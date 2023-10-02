c297f14d2f7b0d22fe0a3000fec1227b5e8ce8e5
Chandansahu18 <chandansahu310702@gmail.com>
2023-10-02 14:01:13 +0530

    files uploaded

--- arrayInJava_revisionPractice.java
+++ arrayInJava_revisionPractice.java
@@ -1,34 +1,74 @@
 import java.util.*;
-
 public class arrayInJava_revisionPractice {
 
-    public void multiDemensionArray(){
 
-     Scanner sc  = new Scanner(System.in);
-     System.out.println("Enter the size of 2-Demensional array");
-     System.out.print("Enter rows: ");
-     int row = sc.nextInt();
-     System.out.print("Enter columns: ");
-     int column = sc.nextInt();
-     int arr[][] = new int[row][column];
-     System.out.print("Enter elements: ");
-     for(int i=0;i<arr.length;i++){
-        for(int j=0;j<arr[i].length;j++){
-             arr[i][j] = sc.nextInt(); //Taking input in Rows & Columns
-        }
-        System.out.println();
-    }
-    
-    System.out.println("Elements in the 2-Demensional array are: ");
+   public void arrMax(){
+    int Max = 0;
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the size of array: ");
+    int size = sc.nextInt();
+    int[] arr = new int[size];
+    System.out.print("Enter the elements  in the array: ");
     for(int i=0;i<arr.length;i++){
-        for(int j=0;j<arr[i].length;j++){
-            System.out.print(arr[i][j]+" "); // output
+        arr[i] = sc.nextInt(); //Taking input 
+    } 
+    for(int i=0;i<arr.length;i++){
+        if(arr[i]>Max){
+         Max = arr[i];
         }
+    }
         System.out.println();
+        System.out.println("Max element is: "+Max);
     }
 
+   
+
+    // public void arrSum(){
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.print("Enter the size in the array: ");
+    //     int size = sc.nextInt();
+    //     int[] arr = new int[size];
+    //     int arrSum = 0;
+    //     System.out.print("Enter the elements in the array: ");
+    //     for(int i=0;i<arr.length;i++){
+    //         arr[i] = sc.nextInt();//taking input
+    //     }
+    //     System.out.print("Array elements are: ");
+    //     for(int i=0;i<arr.length;i++){
+    //         System.out.print(arr[i]+" ");
+    //         arrSum += arr[i]; 
+    //     }
+    //     System.out.println();
+    //     System.out.println("Sum of elements are: "+arrSum);
+
+    // }
+    // public void multiDemensionArray(){
+
+    //  Scanner sc  = new Scanner(System.in);
+    //  System.out.println("Enter the size of 2-Demensional array");
+    //  System.out.print("Enter rows: ");
+    //  int row = sc.nextInt();
+    //  System.out.print("Enter columns: ");
+    //  int column = sc.nextInt();
+    //  int arr[][] = new int[row][column];
+    //  System.out.print("Enter elements: ");
+    //  for(int i=0;i<arr.length;i++){
+    //     for(int j=0;j<arr[i].length;j++){
+    //          arr[i][j] = sc.nextInt(); //Taking input in Rows & Columns
+    //     }
+    //     System.out.println();
+    // }
     
-    }
+    // System.out.println("Elements in the 2-Demensional array are: ");
+    // for(int i=0;i<arr.length;i++){
+    //     for(int j=0;j<arr[i].length;j++){
+    //         System.out.print(arr[i][j]+" "); // output
+    //     }
+    //     System.out.println();
+    // }
+
+    
+    // }
     // public void oneDemensionArray(){
 
     //     Scanner sc = new Scanner(System.in);
@@ -48,6 +88,8 @@ public class arrayInJava_revisionPractice {
         
         arrayInJava_revisionPractice object = new arrayInJava_revisionPractice();
         //object.oneDemensionArray();
-          object.multiDemensionArray();
+        //object.multiDemensionArray();
+        //object.arrSum();
+        object.arrMax();
     }
 }
